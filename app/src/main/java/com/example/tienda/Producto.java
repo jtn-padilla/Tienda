ec6e7a3f7cc1c2358b8aa096f32ff9e86a32f53c
John <jtn.padilla@gmail.com>
2019-11-28 14:37:30 -0600

    Initial commit clase producto

---
+++ app/src/main/java/com/example/tienda/Producto.java
@@ -0,0 +1,64 @@
+package com.example.tienda;
+
+public class Producto {
+    int id;
+    String name, thumbnail_url, provider;
+    float price, delivery;
+
+    public Producto(int id, String name, String thumbnail_url, String provider, float price, float delivery) {
+        this.id = id;
+        this.name = name;
+        this.thumbnail_url = thumbnail_url;
+        this.provider = provider;
+        this.price = price;
+        this.delivery = delivery;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumbnail_url() {
+        return thumbnail_url;
+    }
+
+    public void setThumbnail_url(String thumbnail_url) {
+        this.thumbnail_url = thumbnail_url;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(float delivery) {
+        this.delivery = delivery;
+    }
+}
